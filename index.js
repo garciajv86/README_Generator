fc8ecb80c91abfd7fa2558e1ca8282b157c541ab
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-05-12 12:00:09 -0500

    linked packages and created a question

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // TODO: Include packages needed for this application
+const { questions } = require("./questions");
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+
+function askQuestions() {
+  inquirer.prompt(questions).then((answers) => {
+    console.log(answers);
+  });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  console.log(questions);
+  askQuestions();
+}
 
 // Function call to initialize app
 init();
---
+++ questions.js
@@ -0,0 +1,13 @@
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    type: "input",
+    message: "What is the name of your project?",
+    name: "projectName",
+    default: "README GENERATOR",
+  },
+];
+
+module.exports = {
+  questions,
+};
